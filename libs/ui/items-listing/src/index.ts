0296357f0cec2f5a8ce81bf23e1f6e780790c1df
Michał Łukasiewicz <michal.lukasiewicz@KQ6X9NYJY4.local>
2025-08-22 13:23:22 +0200

    feat(ui/items-listing): add index.ts exports for all items-listing components
    - Export ItemListingContainerDirective
    - Export IListingItem interface
    - Export ListingContainerState class
    - Export IListingSlice and ListingItemMetadata types
    - Export ListingContainerFactory and LoadingListingSliceFactory
    - Export ParamMapToAppListingRequestDtoMapper
    - Enable clean imports via @ui/items-listing alias

---
+++ libs/ui/items-listing/src/index.ts
@@ -0,0 +1,17 @@
+// Directives
+export { ItemListingContainerDirective } from './item-listing-container.directive';
+
+// Interfaces
+export type { IListingItem } from './interfaces/listing-item.interface';
+
+// Models
+export { ListingContainerState } from './models/listing-container.state';
+export type { IListingSlice } from './models/listing-slice';
+export type { ListingItemMetadata } from './models/listing-item-metadata';
+
+// Factories
+export { ListingContainerFactory } from './factories/listing-container.factory';
+export { LoadingListingSliceFactory } from './factories/loading-listing-slice.factory';
+
+// Mappers
+export { ParamMapToAppListingRequestDtoMapper } from './mappings/param-map-to-app-listing-request-dto.mapper';
