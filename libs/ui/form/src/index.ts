033a1fff314d1d6e6c13283775694e9dfafb66ed
Michał Łukasiewicz <michal.lukasiewicz@KQ6X9NYJY4.local>
2025-08-22 13:14:49 +0200

    refactor(ui/form): simplify index.ts to just re-export components

--- libs/ui/form/src/index.ts
+++ libs/ui/form/src/index.ts
@@ -6,15 +6,3 @@ export { InputValidationComponent } from './input-validation/input-validation.co
 
 // Multiselect Dropdown Component
 export { MultiselectDropdownComponent } from './mutliselect-dropdown/multiselect-dropdown.component';
-
-// Import components for the barrel export
-import { ChipCheckboxComponent } from './chip-checkbox/chip-checkbox.component';
-import { InputValidationComponent } from './input-validation/input-validation.component';
-import { MultiselectDropdownComponent } from './mutliselect-dropdown/multiselect-dropdown.component';
-
-// Re-export all components as a barrel export
-export const FORM_COMPONENTS = [
-  ChipCheckboxComponent,
-  InputValidationComponent,
-  MultiselectDropdownComponent
-];
