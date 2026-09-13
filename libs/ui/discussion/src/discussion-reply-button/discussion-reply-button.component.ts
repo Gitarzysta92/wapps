2eac97463fd7252cd1c14558bd2843178223a815
Michał Łukasiewicz <michal.lukasiewicz02@gmail.com>
2026-09-13 18:13:54 +0200

    fix(portal): align comment actions with feed footer styling
    (cherry picked from commit 13826be5f13eea2ace015fc6f63ea0f03ff0731c)

--- libs/ui/discussion/src/discussion-reply-button/discussion-reply-button.component.ts
+++ libs/ui/discussion/src/discussion-reply-button/discussion-reply-button.component.ts
@@ -4,14 +4,13 @@ import {
   input,
   output,
 } from '@angular/core';
-import { TuiButton, TuiIcon } from '@taiga-ui/core';
+import { TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'ui-discussion-reply-button',
   standalone: true,
-  imports: [TuiButton, TuiIcon],
+  imports: [TuiButton],
   templateUrl: './discussion-reply-button.component.html',
-  styleUrls: ['./discussion-reply-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DiscussionReplyButtonComponent {
